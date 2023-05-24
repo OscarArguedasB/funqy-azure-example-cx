3a0175914d45963fcc47df319d370b0402cc90f6
jeremyrdavis <jeremyrdavis@mac.com>
2023-05-24 14:04:21 -0400

    added terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "AZ_LOCATION" {
+  description = "The Azure location where all resources in this example should be created"
+  default     = "eastus"
+}
+
+variable "AZ_RESOURCE_GROUP" {
+  description = "The resource group"
+  default = "funqy-rg"
+}
+
+variable "AZ_FUNCTION_NAME_APP" {
+  description = "The name of the application running functions"
+  default = "funqy-azure-example-runner"
+}
+
+variable "AZ_STORAGE_NAME" {
+  description = "The name of the storage account for the functions"
+  default = "funqysa"
+}
